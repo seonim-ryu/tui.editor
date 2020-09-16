9f6fb9ca7f1badec5976c0a2daa794d7fe272fed
seonim-ryu <seonim.ryu@nhn.com>
2020-09-16 18:28:39 +0900

    wip: add renderer for gfm spec

--- apps/editor/src/js/convertor/rendererMap.js
+++ apps/editor/src/js/convertor/rendererMap.js
@@ -1,4 +1,3 @@
-/* eslint-disable */
 import { backticksFor, isPlainURL } from './stateHelpers';
 
 export const nodes = {
@@ -8,13 +7,13 @@ export const nodes = {
   },
 
   heading(state, node) {
-    state.write(state.repeat('#', node.attrs.level) + ' ');
+    state.write(`${state.repeat('#', node.attrs.level)} `);
     state.renderInline(node);
     state.closeBlock(node);
   },
 
   codeBlock(state, node) {
-    state.write('```' + (node.attrs.params || '') + '\n');
+    state.write(`\`\`\`${node.attrs.params || ''}\n`);
     state.text(node.textContent, false);
     state.ensureNewLine();
     state.write('```');
@@ -22,25 +21,76 @@ export const nodes = {
   },
 
   bulletList(state, node) {
-    state.renderList(node, '  ', () => (node.attrs.bullet || '*') + ' ');
-  },
-
-  blockQuote(state, node) {
-    state.wrapBlock('> ', null, node, () => state.renderContent(node));
+    state.renderList(node, '  ', () => `${node.attrs.bullet || '*'} `);
   },
 
   orderedList(state, node) {
-    let start = node.attrs.order || 1;
-    let maxW = String(start + node.childCount - 1).length;
-    let space = state.repeat(' ', maxW + 2);
+    const start = node.attrs.order || 1;
+    const maxW = String(start + node.childCount - 1).length;
+    const space = state.repeat(' ', maxW + 2);
+
     state.renderList(node, space, i => {
-      let nStr = String(start + i);
-      return state.repeat(' ', maxW - nStr.length) + nStr + '. ';
+      const nStr = String(start + i);
+
+      return `${state.repeat(' ', maxW - nStr.length)}${nStr}. `;
     });
   },
 
   item(state, node) {
+    const { task, checked } = node.attrs;
+
+    if (task) {
+      const marker = `[${checked ? 'x' : ' '}] `;
+
+      state.wrapBlock(marker, null, node, () => state.renderContent(node));
+    } else {
+      state.renderContent(node);
+    }
+  },
+
+  table(state, node) {
     state.renderContent(node);
+    state.closeBlock(node);
+  },
+
+  tableHead(state, node) {
+    const row = node.firstChild;
+    let result = '';
+
+    state.renderContent(node);
+
+    for (let i = 0, len = row.childCount; i < len; i += 1) {
+      const { textContent } = row.child(i);
+
+      result += `| ${state.repeat('-', textContent.length)} `;
+    }
+
+    state.write(`${result}|\n`);
+  },
+
+  tableBody(state, node) {
+    state.renderContent(node);
+  },
+
+  tableRow(state, node) {
+    state.renderContent(node);
+    state.write('| \n');
+  },
+
+  tableHeadCell(state, node) {
+    state.write('| ');
+    state.renderInline(node);
+    state.write(' ');
+  },
+
+  tableBodyCell(state, node) {
+    state.write('| ');
+    state.renderInline(node);
+    state.write(' ');
+  },
+
+  blockQuote(state, node) {
+    state.wrapBlock('> ', null, node, () => state.renderContent(node));
   },
 
   thematicBreak(state, node) {
@@ -49,19 +99,16 @@ export const nodes = {
   },
 
   image(state, node) {
-    state.write(
-      '![' +
-        state.esc(node.attrs.alt || '') +
-        '](' +
-        state.esc(node.attrs.src) +
-        (node.attrs.title ? ' ' + state.quote(node.attrs.title) : '') +
-        ')'
-    );
+    const alt = state.esc(node.attrs.alt || '');
+    const src = state.esc(node.attrs.src);
+    const title = node.attrs.title ? ` ${state.quote(node.attrs.title)}` : '';
+
+    state.write(`![${alt}](${src}${title})`);
   },
 
   hardBreak(state, node, parent, index) {
-    for (let i = index + 1; i < parent.childCount; i++)
-      if (parent.child(i).type != node.type) {
+    for (let i = index + 1; i < parent.childCount; i += 1)
+      if (parent.child(i).type !== node.type) {
         state.write('\\\n');
         return;
       }
@@ -84,12 +131,10 @@ export const marks = {
       return isPlainURL(mark, parent, index, 1) ? '<' : '[';
     },
     close(state, mark, parent, index) {
-      return isPlainURL(mark, parent, index, -1)
-        ? '>'
-        : '](' +
-            state.esc(mark.attrs.href) +
-            (mark.attrs.title ? ' ' + state.quote(mark.attrs.title) : '') +
-            ')';
+      const href = state.esc(mark.attrs.href);
+      const title = mark.attrs.title ? ` ${state.quote(mark.attrs.title)}` : '';
+
+      return isPlainURL(mark, parent, index, -1) ? '>' : `](${href}${title})`;
     }
   },
 
